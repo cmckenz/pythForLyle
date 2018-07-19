135b04262f25b42076e267cb9ab0118cf2881b82
Cameron McKenzie <cmckenz@gmail.com>
2018-07-19 17:34:29 +1000

    Did it suggested way

---
+++ pyChallenge2p2.py
@@ -0,0 +1,8 @@
+pythStr = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+
+intab = "abcdefghijklmnopqrstuvwxyz"
+outtab = "cdefghijklmnopqrstuvwxyzab"
+
+trantab = str.maketrans(intab, outtab)
+
+print(pythStr.translate(trantab))
